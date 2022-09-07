20e385a25ebc6ca4714e96c22e8da77a61ede996
Ger Hobbelt <ger@hobbelt.com>
2022-09-07 17:16:14 +0200

    fix for monolithic build mode

--- test/add-subdirectory-project/main.c
+++ test/add-subdirectory-project/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include "zlib-ng.h"
 
+#if defined(BUILD_MONOLITHIC)
+#define main      zlib_report_example_main
+#endif
+
 int main(void) {
     printf("zlib-ng: %s\n", ZLIBNG_VERSION);
     return 0;
