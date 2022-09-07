e314afc9d74f3290698d34ef57b35b7a9c2442c8
Ger Hobbelt <ger@hobbelt.com>
2022-09-07 17:20:05 +0200

    fix for monolithic build mode

--- test/test_main.cc
+++ test/test_main.cc
@@ -9,7 +9,11 @@ extern "C" {
 #  include "cpu_features.h"
 }
 
-GTEST_API_ int main(int argc, char **argv) {
+#if defined(BUILD_MONOLITHIC)
+#define main      zlib_unittests_main
+#endif
+
+GTEST_API_ int main(int argc, const char **argv) {
   printf("Running main() from %s\n", __FILE__);
   cpu_check_features();
   testing::InitGoogleTest(&argc, argv);
